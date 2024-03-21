a8301ba3c765ea33ae5c3efedf25ac33a4e3437c
Binyam <biatsb5656@gmail.com>
2024-03-20 23:26:32 -0700

    writted a class that inherited from base model

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Class Amenity that inherits from base model"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class Amenity that inherits from base model """
+    name = ""
