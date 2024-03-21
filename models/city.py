cf1f26f55e45a838620dcb17137a9c89ac2f03b0
Binyam <biatsb5656@gmail.com>
2024-03-20 23:25:14 -0700

    a city class that inherits from base model

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class of  city that inherits from base model"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ Class State  that inherits from base model """
+    state_id = ""
+    name = ""
